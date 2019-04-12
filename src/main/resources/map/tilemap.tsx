ec0009d1fa77a96872cbe4b4f39b93b452d267ad
Emile <legendre.emile.27@gmail.com>
2019-04-12 15:09:41 +0200

    Merge tileset and tilemap into one file

--- src/main/resources/map/tilemap.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="tilemap" tilewidth="16" tileheight="16" spacing="1" tilecount="504" columns="28">
- <image source="tilemap.png" width="476" height="305"/>
-</tileset>
