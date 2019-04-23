225e02052818ae76fcec98f99ad730d25705d650
Emile <legendre.emile.27@gmail.com>
2019-04-23 19:45:40 +0200

    Adjust John's spawn and movements

--- src/main/java/re/legend/crowd_simulator/bodies/Map.java
+++ src/main/java/re/legend/crowd_simulator/bodies/Map.java
@@ -36,8 +36,8 @@ public class Map {
 	 */
 	public <T extends AgentBody> T createBody(Class<T> bodyType, UUID agentUUID) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
 		// Body position
-		float x = this.random.nextInt(2000);
-		float y = this.random.nextInt(2000);
+		float x = this.random.nextInt(200);
+		float y = this.random.nextInt(200);
 		
 		// Agent's ID
 		UUID id = agentUUID;
