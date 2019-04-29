25ae8f472e50e2773661ff1017b35b7bb3e7a685
Emile <legendre.emile.27@gmail.com>
2019-04-29 18:57:48 +0200

    Add SimulationObject class

--- src/main/java/re/legend/crowd_simulator/bodies/AgentBody.java
+++ src/main/java/re/legend/crowd_simulator/bodies/AgentBody.java
@@ -4,18 +4,16 @@ import com.badlogic.gdx.math.Vector2;
 import java.util.UUID;
 
 
-public abstract class AgentBody {
+public abstract class AgentBody extends SimulationObject {
 	
 	// Body agent's ID
 	private UUID agentId;
-	// Body's position
-	private Vector2 position;
 	
 	/**
 	 * Constructor with body's position (two floats) and UUID
 	 */
 	public AgentBody(float x, float y, UUID id) {
-		this.position = new Vector2(x, y);
+		super(x, y);
 		this.agentId = id;
 	}
 	
@@ -23,26 +21,8 @@ public abstract class AgentBody {
 	 * Constructor with body's position (vector2) and UUID
 	 */
 	public AgentBody(Vector2 position, UUID id) {
-		this.position = position;
+		super(position);
 		this.agentId = id;
-	}
-	
-	/**
-	 * Sets the body position
-	 * @param x the position of the body on the x axis
-	 * @param y the position of the body on the y axis
-	 */
-	public void setPosition(float x, float y) {
-		this.position.x = x;
-		this.position.y = y;
-	}
-	
-	/**
-	 * Return body's position
-	 * @return the position of the body
-	 */
-	public Vector2 getPosition() {
-		return this.position;
 	}
 	
 	/**
---
+++ src/main/java/re/legend/crowd_simulator/bodies/SimulationObject.java
@@ -0,0 +1,35 @@
+package re.legend.crowd_simulator.bodies;
+
+import com.badlogic.gdx.math.Vector2;
+
+public abstract class SimulationObject {
+	// Object's position
+	private Vector2 position;
+	
+	public SimulationObject(float x, float y) {
+		this.position.x = x;
+		this.position.y = y;
+	}
+	
+	public SimulationObject(Vector2 position) {
+		this.position = position;
+	}
+	
+	/**
+	 * Sets the object's position
+	 * @param x the position of the body on the x axis
+	 * @param y the position of the body on the y axis
+	 */
+	public void setPosition(float x, float y) {
+		this.position.x = x;
+		this.position.y = y;
+	}
+	
+	/**
+	 * Return object's position
+	 * @return the position of the body
+	 */
+	public Vector2 getPosition() {
+		return this.position;
+	}
+}
