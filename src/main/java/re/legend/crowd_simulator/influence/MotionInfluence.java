484dcad4a5f8ab20479e7f1725c1593ea1d14963
Andre <a.aing.pro@gmail.com>
2019-05-04 18:55:13 +0200

    Overload constructors in MotionInfluence for use cases

--- src/main/java/re/legend/crowd_simulator/influence/MotionInfluence.java
+++ src/main/java/re/legend/crowd_simulator/influence/MotionInfluence.java
@@ -1,8 +1,14 @@
 package re.legend.crowd_simulator.influence;
 
+
 import org.lwjgl.util.vector.Vector2f;
 
+import com.badlogic.gdx.math.Vector2;
+
+import io.sarl.jaak.environment.external.body.TurtleBody;
+import io.sarl.jaak.environment.external.perception.EnvironmentalObject;
 import re.legend.crowd_simulator.bodies.AgentBody;
+import re.legend.crowd_simulator.objects.SimulationObject;
 
 
 public class MotionInfluence extends Influence
@@ -11,7 +17,7 @@ public class MotionInfluence extends Influence
 	private Vector2f linearMotion;
 	private float angularMotion;
 	
-	/**
+	/** Main Constructor
 	 * @param emitter is the influencer
 	 * @param linearMotion is the linear motion to apply
 	 * is the number of cells to traverse.
@@ -28,6 +34,73 @@ public class MotionInfluence extends Influence
 		this.linearMotion = linearMotion;
 		this.angularMotion = angularMotion;
 	}
+	
+	/** Constructor when the agent only rotate
+	 * 
+	 * @param emitter
+	 * @param angularMotion
+	 */
+	public MotionInfluence(AgentBody emitter, float angularMotion)
+	{
+		super(emitter);
+		//Test
+		//assert (emitter != null);
+		this.agentB = emitter;
+		this.linearMotion = new Vector2f();
+		this.angularMotion = angularMotion;
+	}
+	
+	/** Constructor when the agent only go forward
+	 * 
+	 * @param emitter
+	 * @param linearMotion
+	 */
+	public MotionInfluence(AgentBody emitter, Vector2f linearMotion)
+	{
+		//Test
+		//assert (emitter != null);
+		//asser (linearMotion != null);
+		super(emitter);
+		this.agentB = emitter;
+		this.linearMotion = linearMotion;
+		this.angularMotion = 0f;
+	}
+	
+	/**Constructor if the agent do not move
+	 * 
+	 * @param emitter
+	 */
+	public MotionInfluence(AgentBody emitter)
+	{
+		//Test
+		//assert (emitter != null);
+		super(emitter);
+		this.agentB = emitter;
+		this.linearMotion = new Vector2f();
+		this.angularMotion = 0f;
+	}
+	
+	/**TO DO?
+	 * Constructor when an object influence translation and rotation
+	 * 
+	 * @param emitter
+	 * @param simObject
+	 * @param linearMotion
+	 * @param angularMotion
+	
+	public MotionInfluence(AgentBody emitter, SimulationObject simObject, Vector2f linearMotion, float angularMotion)
+	{
+		//Test
+		//assert (linearMotion != null);
+		//assert (object != null);
+		super(emitter);
+		
+		this.agentB = simObject;
+		this.linearMotion = linearMotion;
+		this.angularMotion = angularMotion;
+	}
+	*/
+	
 	
 	/**
 	 * @return the moved body
@@ -69,5 +142,24 @@ public class MotionInfluence extends Influence
 	public void setLinearMotion(float angularMotion)
 	{
 		this.angularMotion=angularMotion;
+	}
+	
+	/**
+	 * 
+	 * @return true if the influence has a linear motion
+	 */
+	public boolean hasLinearMotion() 
+	{
+		//lengthSquared for Vector2f value
+		return this.linearMotion != null && this.linearMotion.lengthSquared() != 0f;
+	}
+	
+	/**
+	 * 
+	 * @return true if the influence has a rotation
+	 */
+	public boolean hasAngularMotion()
+	{
+		return this.angularMotion != 0f;
 	}
 }
